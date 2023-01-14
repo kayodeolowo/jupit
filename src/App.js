ac223407228ca7f896d5aa453f21d6aac3d0150a
kayode olowo <officialkayodeolowo@gmail.com>
2023-01-14 07:01:37 -0800

    Header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,53 @@
+import { Header } from "./components";
+
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-red-500"> test </h1>
+    <div>
+      
+      <Header/>
+      <div> Answer misery adieus add wooded how nay men before though. Pretended belonging contented mrs suffering favourite you the continual. Mrs civil nay least means tried drift. Natural end law whether but and towards certain. Furnished unfeeling his sometimes see day promotion. Quitting informed concerns can men now. Projection to or up conviction uncommonly delightful continuing. In appetite ecstatic opinions hastened by handsome admitted.
+
+Dashwood contempt on mr unlocked resolved provided of of. Stanhill wondered it it welcomed oh. Hundred no prudent he however smiling at an offence. If earnestly extremity he he propriety something admitting convinced ye. Pleasant in to although as if differed horrible. Mirth his quick its set front enjoy hoped had there. Who connection imprudence middletons too but increasing celebrated principles joy. Herself too improve gay winding ask expense are compact. New all paid few hard pure she.
+
+Real sold my in call. Invitation on an advantages collecting. But event old above shy bed noisy. Had sister see wooded favour income has. Stuff rapid since do as hence. Too insisted ignorant procured remember are believed yet say finished.
+
+Ask especially collecting terminated may son expression. Extremely eagerness principle estimable own was man. Men received far his dashwood subjects new. My sufficient surrounded an companions dispatched in on. Connection too unaffected expression led son possession. New smiling friends and her another. Leaf she does none love high yet. Snug love will up bore as be. Pursuit man son musical general pointed. It surprise informed mr advanced do outweigh.
+
+Wise busy past both park when an ye no. Nay likely her length sooner thrown sex lively income. The expense windows adapted sir. Wrong widen drawn ample eat off doors money. Offending belonging promotion provision an be oh consulted ourselves it. Blessing welcomed ladyship she met humoured sir breeding her. Six curiosity day assurance bed necessary.
+
+Quick six blind smart out burst. Perfectly on furniture dejection determine my depending an to. Add short water court fat. Her bachelor honoured perceive securing but desirous ham required. Questions deficient acuteness to engrossed as. Entirely led ten humoured greatest and yourself. Besides ye country on observe. She continue appetite endeavor she judgment interest the met. For she surrounded motionless fat resolution may.
+
+Advanced extended doubtful he he blessing together. Introduced far law gay considered frequently entreaties difficulty. Eat him four are rich nor calm. By an packages rejoiced exercise. To ought on am marry rooms doubt music. Mention entered an through company as. Up arrived no painful between. It declared is prospect an insisted pleasure.
+
+John draw real poor on call my from. May she mrs furnished discourse extremely. Ask doubt noisy shade guest did built her him. Ignorant repeated hastened it do. Consider bachelor he yourself expenses no. Her itself active giving for expect vulgar months. Discovery commanded fat mrs remaining son she principle middleton neglected. Be miss he in post sons held. No tried is defer do money scale rooms.
+
+Day handsome addition horrible sensible goodness two contempt. Evening for married his account removal. Estimable me disposing of be moonlight cordially curiosity. Delay rapid joy share allow age manor six. Went why far saw many knew. Exquisite excellent son gentleman acuteness her. Do is voice total power mr ye might round still.
+
+Sex and neglected principle ask rapturous consulted. Object remark lively all did feebly excuse our wooded. Old her object chatty regard vulgar missed. Speaking throwing breeding betrayed children my to. Me marianne no he horrible produced ye. Sufficient unpleasing an insensible motionless if introduced ye. Now give nor both come near many late.
+
+
+
+What does the Lorem Ipsum text mean?
+Lorem Ipsum comes from a latin text written in 45BC by Roman statesman, lawyer, scholar, and philosopher, Marcus Tullius Cicero. The text is titled "de Finibus Bonorum et Malorum" which means "The Extremes of Good and Evil". The most common form of Lorem ipsum is the following:
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+
+The text is a corrupted version of the original and therefore does not mean anything in particular. The book however where it originates discusses the philosophical views of Epicureanism, Stoicism, and the Platonism of Antiochus of Ascalon.
+
+Lorem ipsum is widely in use since the 14th century and up to today as the default dummy "random" text of the typesetting and web development industry. In fact not only it has survived the test of time but it thrived and can be found in many software products, from Microsoft Word to WordPress.
+
+
+
+What is random text generator?
+Random Text Generator is a web application which provides true random text which you can use in your documents or web designs. How does it work? First we took many books available on project Gutenberg and stored their contents in a database. Then a computer algorithm takes the words we stored earlier and shuffles them into sentences and paragraphs.
+
+The algorithm takes care to create text that looks similar to an ordinary book but without any real meaning. The reason we want our text to be meaningless is that we want the person viewing the resulting random text to focus on the design we are presenting, rather than try to read and understand the text.
+
+It's better than Lorem ipsum because it can produce text in many languages and in particular: Chinese, Dutch, English, Finnish, French, German, Greek, Hebrew, Italian, Japanese, Latin, Polish, Portuguese, Russian, Serbian and Spanish.
+
+Also when you use plain Lorem ipsum text, your design will look like a million other designs out there. With Random Text Generator your designs will look more unique while still containing text which truly means nothing.
+
+</div>
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,49 @@
+import {AiOutlineClose, AiOutlineMenuUnfold}  from 'react-icons/ai'
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+
+
+function Header() {
+  const [open, setOpen] = useState(false); 
+  const handleNav = () => {
+    setOpen(!open);
+  };
+
+  return (
+    <div className='h-14 fixed flex items-center justify-between px-5 top-0 left-0 right-0 z-10 bg-red-600'>
+      <h1> LOGO </h1>
+
+      <div className='model-links hidden lg:flex items-center font-semibold uppercase flex-nowrap '>
+       <ul> 
+         <li> Home </li>
+         <li> Features </li>
+         <li> About </li>
+       </ul>
+      </div>
+
+    
+    
+
+    {/* mobile menu */}
+      <div className=' flex items-center font-semibold uppercase'>
+        <div onClick={() => setOpen(!open)} className=' lg:hidden cursor-pointer z-30'>
+          {open ? <AiOutlineClose/> :  <AiOutlineMenuUnfold/>}
+        </div>
+      </div>
+
+      <ul onClick={handleNav}
+        className={`fixed top-14 bottom-0 right-0 h-full bg-blue-400 z-20 w-full list-none p-5 flex flex-col transition-all duration-700 ease-in  ${
+          open ? 'opacity-100' : ' -top-80 opacity-0'
+        }`}
+      >
+       
+        <Link to='/shop'> <li> shop </li>  </Link>
+        <Link to='/shop'> <li> about </li>  </Link>
+        <Link to='/shop'> <li> Features </li>  </Link>
+      </ul>
+    </div>
+  );
+}
+
+export default Header;
